0e4a90feb161f85e99a62fa61ac0accd3e61bd5c
AngieOgutu <angieogutu01@gmail.com>
2022-03-11 11:59:52 -0800

    Task 4 betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
- *main - Prints the phrase "Programming is like building a multilingual puzzle"
- *Returns: 0 if successful and non zero if not
+ * main - Prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if successful and non zero if not
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
