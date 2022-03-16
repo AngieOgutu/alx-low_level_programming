266b2c355beeabbf653e0c8a72e61e4293350691
AngieOgutu <angieogutu01@gmail.com>
2022-03-16 12:24:54 -0700

    Editing description for return value

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lower case charachter
  * @c : character to check
- * Return : (0) if lowercase, (1) if otherwise
+ * Return: (1) if lowercase, (0) if otherwise
  */
 
 int _islower(int c)
