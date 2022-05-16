c97559fbd226c5a789715130643f76d1093ded5c
AngieOgutu <angieogutu01@gmail.com>
2022-05-16 07:30:11 -0700

    Print largest edit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int main ()
 	int c;
 	
 
-	if (a >= b && b >= c)
+	if (a >= b && a >= c)
 
 	{
 		printf ("%d", a);
 	}
 
-	else if (b >= a && a >= c)
+	else if (b >= a && b >= c)
 
 	{
 		printf ("%d", b);
