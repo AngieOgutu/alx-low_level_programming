b55204fe74c9c2685e55b0dedd3344ae6bf10295
AngieOgutu <angieogutu01@gmail.com>
2023-10-21 05:54:58 +0000

    Declare at the beginning

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,38 +11,40 @@
 
 int main(int argc, char *argv[])
 {
+	int cents, coins25, coins10, coins5, coins2, coins1, mincoins;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 1)
 	{
 		printf("0\n");
 	}
 
-	int coins25 = cents / 25;
+	coins25 = cents / 25;
 
 	cents %= 25;
 
-	int coins10 = cents / 10;
+	coins10 = cents / 10;
 
 	cents %= 10;
 
-	int coins5 = cents / 5;
+	coins5 = cents / 5;
 
 	cents %= 5;
 
-	int coins2 = cents / 2;
+	coins2 = cents / 2;
 
 	cents %= 2;
 
-	int coins1 = cents;
+	coins1 = cents;
 
-	int mincoins = coins25 + coins10 + coins5 + coins2 + coins1;
+	mincoins = coins25 + coins10 + coins5 + coins2 + coins1;
 
 	printf("%d\n", mincoins);
 	return (0);
