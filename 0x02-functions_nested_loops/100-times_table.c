f42ab861f00fbf89d78678ac16755024e9686cff
AngieOgutu <angieogutu01@gmail.com>
2023-10-25 06:19:50 +0000

    Braces placement

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,35 +7,32 @@
   * Return: 0
   */
 
-void print_times_table(int n)
-{
+void print_times_table(int n) {
 	int i, j, prod;
 
-	if (n > 15 || n < 0)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
+	if (n > 15 || n < 0) {
+		for (i = 0; i <= n; i++) {
+			for (j = 0; j <= n; j++) {
 				prod = i * j;
 
-			if (prod > 99)
-			_putchar((prod / 100) + '0');
-			_putchar((prod / 10 % 10) + '0');
-			_putchar((prod % 10) + '0');
-			else if (prod > 9)
-			_putchar(' ');
-			_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
-			else if (j != 0)
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(prod + '0');
-			else
-			_putchar(prod + '0');
+			if (prod > 99) {
+				_putchar((prod / 100) + '0');
+				_putchar((prod / 10 % 10) + '0');
+				_putchar((prod % 10) + '0');
+			} else if (prod > 9) {
+				_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			} else if (j != 0) {
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			} else {
+				_putchar(prod + '0');
+			}
 			if (j != n)
-			_putchar(',');
-			_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
