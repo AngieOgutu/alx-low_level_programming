85016ab0b377ab3d0c4f7b2035c0451f60f477b9
AngieOgutu <angieogutu01@gmail.com>
2023-10-25 06:01:13 +0000

    Indentation

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,23 +18,23 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 				prod = i * j;
 
-				if (prod > 99)
-					_putchar((prod / 100) + '0');
-					_putchar((prod / 10 % 10) + '0');
-					_putchar((prod % 10) + '0');
-				else if (prod > 9)
-					_putchar(' ');
-					_putchar((prod / 10) + '0');
-					_putchar((prod % 10) + '0');
-				else if (j != 0)
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(prod + '0');
-				else
-					_putchar(prod + '0');
-				if (j != n)
-					_putchar(',');
-					_putchar(' ');
+			if (prod > 99)
+				_putchar((prod / 100) + '0');
+				_putchar((prod / 10 % 10) + '0');
+				_putchar((prod % 10) + '0');
+			else if (prod > 9)
+				_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			else if (j != 0)
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			else
+				_putchar(prod + '0');
+			if (j != n)
+				_putchar(',');
+				_putchar(' ');
 			_putchar('\n');
 		}
 	}
