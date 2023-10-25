2ae0dfceeb10af0f175afdff772579875ba6fc99
AngieOgutu <angieogutu01@gmail.com>
2023-10-25 06:21:05 +0000

    Brace placement

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,9 +30,10 @@ void print_times_table(int n) {
 			} else {
 				_putchar(prod + '0');
 			}
-			if (j != n)
+			if (j != n){
 				_putchar(',');
 				_putchar(' ');
+			}
 			}
 			_putchar('\n');
 		}
